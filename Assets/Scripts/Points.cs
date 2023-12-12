57b98febaba12c8a0de17322acb26328e49aa25f
Jan Sambor <janeksambor@gmail.com>
2023-12-12 13:56:18 +0100

    sus

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -10,13 +10,18 @@ public class Points : MonoBehaviour
     public int points;
     public float pointMultiplier;
     public bool autoClick;
-    
-    public event Action<int> OnPointsChanged = delegate {  };
-    
+
+    public event Action<int> OnPointsChanged = delegate { };
+
     public void Click()
     {
         points += (int)(1 * pointMultiplier);
         OnPointsChanged(points);
+    }
+
+    public void setAutoclicker(bool autoClick)
+    {
+        this.autoClick = autoClick;
     }
 
     private void Update()
